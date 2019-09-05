6ebe5c7e8b78aad3a81d433688681def82ddc4eb
damxin <nfx080523@hotmail.com>
2019-09-05 13:46:52 +0800

    表创建sql自动生成

--- DataBase/generadatabase.py
+++ DataBase/generadatabase.py
@@ -16,6 +16,23 @@ VARCHINAMECOL = 2
 VARDICTCOL = 3
 VARCOLS = 4
 FIRSTVARSTART = "varengname"
+TABLENAME = "tablename"
+CREATETABLEHEAD="DROP PROCEDURE IF EXISTS sp_db_mysql; DELIMITER $$ CREATE PROCEDURE sp_db_mysql() BEGIN  DECLARE v_rowcount INT;  DECLARE database_name VARCHAR(100);  SELECT DATABASE() INTO database_name;  SELECT COUNT(1) INTO v_rowcount FROM information_schema.tables WHERE table_schema= database_name AND table_name='%s';  IF v_rowcount = 0 THEN"
+CREATETABLETAIL="END IF;  END$$ DELIMITER ; CALL sp_db_mysql(); DROP PROCEDURE IF EXISTS sp_db_mysql;"
+ADDVARHEAD = "DROP PROCEDURE IF EXISTS sp_db_mysql; \
+DELIMITER $$ \
+	CREATE PROCEDURE sp_db_mysql() \
+		BEGIN \
+			declare v_rowcount int; \
+			declare database_name VARCHAR(100); \
+			select database() into database_name; \
+			select count(1) into v_rowcount from information_schema.columns where table_schema= database_name and table_name='%s' and column_name='%s'; \
+			if v_rowcount = 0 then"
+ADDVARTAIL = "end if; \
+		END$$ \
+DELIMITER ; \
+	call sp_db_mysql(); \
+DROP PROCEDURE IF EXISTS sp_db_mysql;"
 
 '''
     第一个sheet字段读取,前4列
@@ -47,12 +64,33 @@ def readvarxlsx(q):
 '''
     读取表生成表创建脚本
 '''
-def generyCreateSql():
+def generyCreateSql(vardict):
     workbook = xlrd.open_workbook(FILE_DIR)
-    sizesheet = workbook.sheets().length()
+    sizesheet = workbook.sheets().length() - 1
+    starttablecol = 0
+    starttablerow = 0
     for indexsheet in range(sizesheet):      
-        booksheet = workbook.sheet_by_index(indexsheet+1)
-
+        tablesheet = workbook.sheet_by_index(indexsheet+1)
+        startflag = 0
+        for col in range(tablesheet.ncols):
+            for row in range(tablesheet.nrows):
+                cel = tablesheet.cell(row, col)
+                celreal = cel.lower().trim()
+                if celreal in TABLENAME:
+                    startflag = 1
+                    starttablecol = col
+                    starttablerow = row
+                if startflag == 1:                    
+                    gentablename = tablesheet.cell(starttablerow, starttablecol+1)
+                    createtablesql = CREATETABLEHEAD % gentablename
+                    for tblrow in range(starttablerow, tablesheet.nrows):
+                        cel = tablesheet.cell(tblrow, starttablecol)
+                        varName = cel.lower().trim()
+                        cel = tablesheet.cell(tblrow, starttablecol+1)
+                        varAddFlag = cel.lower().trim()
+                    
+            
+            
     
 if __name__ == '__main__':
     
