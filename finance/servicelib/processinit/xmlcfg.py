1e0538b793f37f24998e4c28c06abf77fd0bebda
damxin <nfx080523@hotmail.com>
2019-09-10 22:25:26 +0800

    xml配置读取

--- finance/servicelib/processinit/xmlcfg.py
+++ finance/servicelib/processinit/xmlcfg.py
@@ -6,3 +6,50 @@ Created on 2019/09/04
 @contact: nfx080523@hotmail.com
 '''
 
+from xml.dom.minidom import parse
+import xml.dom.minidom
+
+from finance.util import GlobalCons as gc
+
+DBDSTAG="ds"
+LOGICNAMEATTR="logic_name"
+DBTYPEATTR="db_type"
+SERVERATTR="server"
+PORTATTR="port"
+USERNAMEATTR="user_name"
+PASSWORDATTR="password"
+DATABASEATTR="database"
+
+class XMLCFG:
+    def __init__(self,xmlfilepath):
+        self.xmlfilepath = xmlfilepath
+        self.dbcfgdict={}
+
+    def getDbInfoFromXmlFile(self):
+        DOMTree = xml.dom.minidom.parse(self.xmlfilepath)
+        collection = DOMTree.documentElement
+        dss = collection.getElementsByTagName(DBDSTAG)
+        for ds in dss:
+            dsdict = {}
+            if ds.hasAttribute(DBTYPEATTR):
+                dsdict[gc.DBTYPEKEY] = ds.getAttribute(DBTYPEATTR)
+            if ds.hasAttribute(SERVERATTR):
+                dsdict[gc.SERVERKEY] = ds.getAttribute(SERVERATTR)
+            if ds.hasAttribute(PORTATTR):
+                dsdict[gc.PORTKEY] = ds.getAttribute(PORTATTR)
+            if ds.hasAttribute(USERNAMEATTR):
+                dsdict[gc.USERNAMEKEY] = ds.getAttribute(USERNAMEATTR)
+            if ds.hasAttribute(PASSWORDATTR):
+                dsdict[gc.PASSWORDKEY] = ds.getAttribute(PASSWORDATTR)
+            if ds.hasAttribute(DATABASEATTR):
+                dsdict[gc.DATABASEKEY] = ds.getAttribute(DATABASEATTR)
+            if ds.hasAttribute(LOGICNAMEATTR):
+                logicnamecfg = ds.getAttribute(LOGICNAMEATTR)
+                self.dbcfgdict[logicnamecfg]=dsdict
+        # print(self.dbcfgdict)
+
+
+
+# if __name__ == '__main__':
+#     dbcfg = XMLCFG("F:\\nfx\\Python\\stockmarket\\finance\\resource\\finance.xml")
+#     dbcfg.getDbInfoFromXmlFile()
---
+++ finance/util/GlobalCons.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 2019/09/04
+@author: damxin
+@group :
+@contact: nfx080523@hotmail.com
+'''
+
+LOGICNAMEKEY="logic_name"
+DBTYPEKEY="db_type"
+SERVERKEY="server"
+PORTKEY="port"
+USERNAMEKEY="user_name"
+PASSWORDKEY="password"
+DATABASEKEY="database"
