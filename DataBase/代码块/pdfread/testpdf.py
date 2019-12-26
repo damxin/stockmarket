5cc9a8b170001285fc315100a7a347d6abefb204
damxin <nfx080523@hotmail.com>
2019-12-26 19:29:38 +0800

    pdf读取和年报文件获取

---
+++ DataBase/代码块/pdfread/testpdf.py
@@ -0,0 +1,20 @@
+import pdfplumber
+import pandas as pd
+
+with pdfplumber.open("通策医疗：2016年年度报告.PDF") as pdf:
+    # 获取第一页
+    first_page = pdf.pages[5]
+	
+	# 解析文本
+    text = first_page.extract_text()
+    print(text)
+	
+	# 解析表格
+    tables = first_page.extract_tables()
+    for table in tables:
+        print(table)
+        # df = pd.DataFrame(table[1:], columns=table[0])
+        for row in table:
+            for cell in row:
+                print(cell, end="\t|")
+            print()
